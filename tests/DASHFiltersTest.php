c3be1069ed8e20ec13165cdefdf13c04b98e4b9d
Amin <amin69y@gmail.com>
2020-02-07 18:37:39 +0330

    update test

--- tests/DASHFiltersTest.php
+++ tests/DASHFiltersTest.php
@@ -30,12 +30,12 @@ class DASHFiltersTest extends TestCase
 
         $this->assertEquals(
             [
-                "-bf", "1", "-keyint_min", "120", "-g", "120"
-                , "-sc_threshold", "0", "-b_strategy", "0", "-strict", "-2", "-use_timeline"
-                , "1", "-use_template", "1", "-f", "dash", "-map", "0", "-b:v:0"
-                , "103k", "-s:v:0", "256x144", "-map", "0", "-b:v:1", "138k"
-                , "-s:v:1", "426x240", "-map", "0", "-b:v:2", "207k", "-s:v:2", "640x360"
-                , "-adaptation_sets", "id=0,streams=v id=1,streams=a"
+                "-bf", "1", "-keyint_min", "120", "-g", "120", "-sc_threshold", "0", "-b_strategy", "0", "-use_timeline", "1",
+                "-use_template", "1", "-init_seg_name", "test_init_\$RepresentationID$.\$ext$",
+                "-media_seg_name", "test_chunk_\$RepresentationID\$_\$Number%05d$.\$ext$", "-seg_duration", "10",
+                "-f", "dash", "-map", "0", "-b:v:0", "103k", "-s:v:0", "256x144", "-map", "0", "-b:v:1", "138k",
+                "-s:v:1", "426x240", "-map", "0", "-b:v:2", "207k", "-s:v:2", "640x360",
+                "-adaptation_sets", "id=0,streams=v id=1,streams=a", "-strict", "-2"
             ],
             $apply);
     }
